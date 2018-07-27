6a6ce043d348bb5985f3697f74bc2c7abc0bed66
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-27 16:57:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,7 @@
 # name = "fido","snoopy","lassie"
   
   class Dog
-    attr_writer
-    def initialize(fido, snoopy, lassie)
-    @fido = fido 
-    @snoopy = snoopy
-    @lassie = lassie
   end
-end 
-def fido
-  @fido 
-end
+  
 dog_one = Dog.new("fido","snoopy","lassie")
 puts dog_one.name
